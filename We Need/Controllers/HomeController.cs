5b515fd3052b9396f4c5332261fdd9b2a5418614
Neuronumort <34415096+Neuronumort@users.noreply.github.com>
2019-05-17 16:45:33 +0300

    Add project files.

---
+++ We Need/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace We_Need.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult Needs()
+        {
+          
+
+            
+            var nume = new List<string>();
+            nume.Add("Marius");
+            return View(nume);
+            
+        }
+
+
+    }
+}
---
+++ We Need/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(We_Need.Startup))]
+namespace We_Need
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
